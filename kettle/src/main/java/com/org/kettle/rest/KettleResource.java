4069f4fa6da6913e3177f3c45681cf43dce519a1
SusanChung <susan.chung@iisigroup.com>
2024-01-02 21:48:24 +0800

    添加springSecurity

--- kettle/src/main/java/com/org/kettle/rest/KettleResource.java
+++ kettle/src/main/java/com/org/kettle/rest/KettleResource.java
@@ -4,9 +4,11 @@ import com.kettle.service.dto.JobDTO;
 import com.kettle.util.WebResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.support.BasicAuthenticationInterceptor;
+//import org.springframework.http.client.support.BasicAuthenticationInterceptor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,6 +16,10 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import java.io.StringReader;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Base64;
+import java.util.Date;
 
 @RestController
 @RequestMapping("/api")
@@ -42,30 +48,30 @@ public class KettleResource {
     //參考官方文件
     //https://help.hitachivantara.com/Documentation/Pentaho/Data_Integration_and_Analytics/9.1/Developer_center/REST_API_Reference/Carte/030#POSTexecuteJob
     public void executeRestApi(JobDTO jobdto) {
-        String jobFilePath = jobdto.getJobFilePath();
-//        String apiUrl = "http://localhost:8080/kettle/executeJob/?job=/pentaho/pdi-ce-9.4.0.0-343/data-integration/job/job_1.kjb";
-        String baseUrl = "http://localhost:8080/kettle/executeJob/";
-        String apiUrl = baseUrl + "?job=" + jobFilePath;
-        String user = "cluster";
-        String password = "cluster";
-        HttpHeaders headers = new HttpHeaders();
-        headers.setBasicAuth(user, password);
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.getInterceptors().add(new BasicAuthenticationInterceptor(user, password));
-        ResponseEntity<String> response = restTemplate.getForEntity(apiUrl, String.class);
-        if (response.getStatusCode().is2xxSuccessful()) {
-            String responseBody = response.getBody();
-            log.info("呼叫CARTE API 成功，responseBody為:{}: ", responseBody);
-            WebResult webResult = getXmlData(responseBody);
-            String result = webResult.getResult();
-            String message = webResult.getMessage();
-            String id = webResult.getId();
-            log.info("呼叫CARTE API 成功，Result: {}", result);
-            log.info("呼叫CARTE API 成功，Message: {}", message);
-            log.info("呼叫CARTE API 成功，Job ID: {}", id);
-        } else {
-            log.error("呼叫CARTE API 失敗，http code為: " + response.getStatusCodeValue());
-        }
+//        String jobFilePath = jobdto.getJobFilePath();
+////        String apiUrl = "http://localhost:8080/kettle/executeJob/?job=/pentaho/pdi-ce-9.4.0.0-343/data-integration/job/job_1.kjb";
+//        String baseUrl = "http://localhost:8080/kettle/executeJob/";
+//        String apiUrl = baseUrl + "?job=" + jobFilePath;
+//        String user = "cluster";
+//        String password = "cluster";
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.setBasicAuth(user, password);
+//        RestTemplate restTemplate = new RestTemplate();
+//        restTemplate.getInterceptors().add(new BasicAuthenticationInterceptor(user, password));
+//        ResponseEntity<String> response = restTemplate.getForEntity(apiUrl, String.class);
+//        if (response.getStatusCode().is2xxSuccessful()) {
+//            String responseBody = response.getBody();
+//            log.info("呼叫CARTE API 成功，responseBody為:{}: ", responseBody);
+//            WebResult webResult = getXmlData(responseBody);
+//            String result = webResult.getResult();
+//            String message = webResult.getMessage();
+//            String id = webResult.getId();
+//            log.info("呼叫CARTE API 成功，Result: {}", result);
+//            log.info("呼叫CARTE API 成功，Message: {}", message);
+//            log.info("呼叫CARTE API 成功，Job ID: {}", id);
+//        } else {
+//            log.error("呼叫CARTE API 失敗，http code為: " + response.getStatusCodeValue());
+//        }
     }
 
     public WebResult getXmlData(String responseXML) {
@@ -78,4 +84,27 @@ public class KettleResource {
         }
         return null;
     }
+
+
+    public static void main(String[] args) {
+        String baseUrl = "http://localhost:8888/api/kettle/test";
+        String jwtToken = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOiIxIiwiaWF0IjoxNzAzODQxNDY5fQ.a6ro9xuzWZDx4cOmK85PDagEpJ3pORoIrzNS4Sx-rfY";
+
+        // Create headers with the JWT token
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + jwtToken);
+
+        // Create an HttpEntity with the headers
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        // Create a RestTemplate
+        RestTemplate restTemplate = new RestTemplate();
+
+        // Perform a POST request
+        ResponseEntity<String> response = restTemplate.exchange(baseUrl, HttpMethod.POST, entity, String.class);
+
+        // Print the response
+        System.out.println("Response: " + response.getBody());
+    }
+
 }
